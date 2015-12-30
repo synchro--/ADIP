fbf18ad86ea0a12d53a9965ba69c7b9b85d8646c
msilosviu <maria_silos@hotmail.com>
2015-12-30 09:46:19 +0100

    small change
    still doesn't work

--- more_test.m
+++ more_test.m
@@ -20,6 +20,7 @@ gradient_dens=zeros(rows,cols);
 
 % Histograms of the central part (without taking into account the 5 pixels
 % borders)
+
 for r=5:rows-5 
     for c=5:cols-5
         fprintf('raw= %d',r)
@@ -30,8 +31,9 @@ for r=5:rows-5
         new_im_down=im(r+1:r+5,c-4:c+5); % cut the image in order to have the lower part
         [counts,~]=imhist(new_im_down,num_bins);
         down_hist=counts;
+        sum_val=0;
         for i=1:length(counts)
-            sum_val= (up_hist(i)-down_hist(i))^2/(up_hist(i)+down_hist(i));
+            sum_val= sum_val+(up_hist(i)-down_hist(i))^2/(up_hist(i)+down_hist(i));
         end
         gradient_dens(r,c)=0.5*sum_val;
     end
@@ -40,7 +42,7 @@ end
 gauss=fspecial('gaussian',8,1); %% Initialized a gaussian filter with sigma=0.5 * block width.    
 
 % % Remove redundant pixels in an image. 
-gradient_dens(gradient_dens<30)=0;
+gradient_dens(gradient_dens<10)=0;
 
 % Show gradient
 figure
