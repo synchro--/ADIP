e38e0017c028d15ba8202ab24431bd66b567e352
msilosviu <maria_silos@hotmail.com>
2016-01-02 22:08:47 +0100

    new results using XY and 45º direction

--- Code/histograms_test.m
+++ Code/histograms_test.m
@@ -1,7 +1,6 @@
 % Code implementing basic idea of the Arbelaez's algorithm 
 
 % Authors: Alessio Salman, María Silos
-
 clc
 clear all
 close all
@@ -14,6 +13,8 @@ root_path = pwd;
 path_im=[root_path filesep 'Images' filesep]; 
 image = 'im_larger.jpg';
 im=imread([path_im image]);
+% im=imrotate(im,45);% for the gradient in the 45º direction. It gives worst
+% results and same efficiency
 num_bins=20;
 neighbors=5; % Number of neighbouring pixels
 
@@ -45,11 +46,10 @@ for r=5:rows-5
         new_im_down=im(r+1:r+5,c-4:c+5); % cut the image in order to have the lower part
         [counts_x,~]=imhist(new_im_down,num_bins);
         down_hist=counts_x;
-%         counts_x(counts_x==0)=1; 
-        sum_val_x=0;
-        for i=1:length(counts_x)
-            sum_val_x= sum_val_x+(up_hist(i)-down_hist(i))^2/(up_hist(i)+down_hist(i));
-        end
+        counts_x(counts_x==0)=1; 
+        % using this instead of a for loop change the results (don't know
+        % why). And decrease the time 5 seconds
+        sum_val_x=sum((up_hist-down_hist).^2./(up_hist+down_hist));
         gradient_magnitude_x=0.5*sum_val_x;
         
         % Oriented Histogram in Y
@@ -61,35 +61,54 @@ for r=5:rows-5
         [counts_y,~]=imhist(new_im_right,num_bins);
         right_hist=counts_y;
 %         counts_y(counts_y==0)=1;
-        sum_val_y=0;
-        for j=1:length(counts_y)
-            sum_val_y= sum_val_y+(left_hist(i)-right_hist(i))^2/(left_hist(i)+right_hist(i));
-        end
+        sum_val_y= sum((left_hist-right_hist).^2./(left_hist+right_hist));
         gradient_magnitude_y=0.5*sum_val_y;
         % Max val of both
-        gradient_dens_sum(r,c)=gradient_magnitude_x + gradient_magnitude_y;
         gradient_dens_max(r,c)=max(gradient_magnitude_x, gradient_magnitude_y);
+        gradient_dens_x(r,c)=gradient_magnitude_x;
+        gradient_dens_y(r,c)=gradient_magnitude_y;
+
 
     end
 end
 
 % gauss=fspecial('gaussian',8,1); %% Initialized a gaussian filter with sigma=0.5 * block width.
 
-% % Remove redundant pixels in an image.
-gradient_dens_sum(gradient_dens_sum<10)=0;
-gradient_dens_max(gradient_dens_max<10)=0;
+% final=imrotate(gradient_dens_x,-45)
+% final=imcrop(final) % you have to manually crop the image 
+% imshow(uint8(final));
+% title('Gradient Density in 45º')
 
-% sgolayfilt(gradient_dens,2)
-% Show gradient
-figure
+% Show gradients
+figure(1)
 subplot(1,3,1)
-imshow(uint8(gradient_dens_sum));
-title('gradient density with sum')
-filtered=sgolayfilt(gradient_dens_max,2,7)
+imshow(uint8(gradient_dens_y));
+title('Gradient Density in Y')
 subplot(1,3,2)
-imshow(uint8(gradient_dens_max));
-title('gradient density with max')
+imshow(uint8(gradient_dens_x));
+title('Gradient Density in X ')
 subplot(1,3,3)
 imshow(uint8(gradient_dens_max));
-title('filtered image')
+title('Gradient Density in X and Y')
+filtered=sgolayfilt(gradient_dens_max,2,7)
+
+figure(2)
+subplot(1,2,1)
+gradient_dens_max(gradient_dens_max<10)=0;
+imshow(uint8(gradient_dens_max));
+title('filtered val<10')
+subplot(1,2,2)
+gradient_dens_max(gradient_dens_max<20)=0;
+imshow(uint8(gradient_dens_max));
+title('filtered val<20')
+
+figure(3)
+subplot(1,2,1)
+gradient_dens_max(gradient_dens_max<25)=0;
+imshow(uint8(gradient_dens_max));
+title('filtered val<25')
+subplot(1,2,2)
+imshow(uint8(filtered));
+title('filtered image_sgolayfilt')
+
 toc
